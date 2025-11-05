ba5f72e5899f2f65f2ca8a82962782042fdc71d2
Mike <mrlawler@protonmail.com>
2025-11-05 11:10:10 -0500

    Added bluepark to work out the blue park opmodes

---
+++ Simulator/src/main/java/com/example/simulator/Bluepark.java
@@ -0,0 +1,80 @@
+package com.example.simulator;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Bluepark {
+    private static final double wait = 1;
+
+    /*
+Red Goal.  Set up to goal side of centerline tile
+-62,15,0  ->  -12,32,30
+
+
+Red Far.  Set up to goal side of centerline tile
+62,15,180 ->  42,18,180
+
+
+Blue Goal.  Set up to goal side of centerline tile
+-62,-14,0 ->  0,-20,-30
+
+Blue Far.
+62,-14,180  ->  44,-18,200
+
+    */
+
+
+    //==============================================
+    public static RoadRunnerBotEntity examplePath( MeepMeep meepMeep)
+    {
+        RoadRunnerBotEntity path = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(
+
+
+
+                            new Pose2d(62, -14, Math.toRadians(180)))
+                        .waitSeconds( wait )
+
+                            .splineTo(new Vector2d(44,-18),Math.toRadians(200))
+
+
+
+                        .build());
+
+        return path;
+    }
+    //==============================================
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+        meepMeep.setMouseCoordinateDisplayPosition( 20, 200 );
+        meepMeep.setShowFPS( false );
+
+        RoadRunnerBotEntity path = examplePath( meepMeep );
+
+        File file = new File("Simulator/decodeField.png");
+        try
+        {
+            BufferedImage image = ImageIO.read( file );
+            meepMeep.setBackground( image )
+                    .setDarkMode( true )
+                    .setBackgroundAlpha( 0.7f )
+                    .addEntity( path )
+                    .start();
+        }
+        catch(Exception e)
+        {
+            System.out.println( e.getMessage() );
+        }
+    }
+}
