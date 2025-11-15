41ba6635a186bf5ac280effb6c3e745ec2b39fb1
Mike <mrlawler@protonmail.com>
2025-11-14 22:45:31 -0500

    Blueshoot auto for purposes of discussion

---
+++ Simulator/src/main/java/com/example/simulator/Blueshoot.java
@@ -0,0 +1,105 @@
+package com.example.simulator;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Blueshoot {
+    private static final double wait = 1;
+
+    /*
+Red Goal.  Set up to goal side of centerline tile
+-62,15,0  ->  -12,32,30
+
+
+Red Far.  Set up to goal side of centerline tile
+62,15,180 ->  42,18,180
+
+
+Blue Goal.  Set up to goal side of centerline tile
+-62,-14,0 ->  0,-20,-30
+
+Blue Far.
+62,-14,180  ->  44,-18,200
+
+    */
+
+
+    //==============================================
+    public static RoadRunnerBotEntity examplePath( MeepMeep meepMeep)
+    {
+        RoadRunnerBotEntity path = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(
+
+
+
+                            new Pose2d(-55, -44, Math.toRadians(230)))
+                        //Shooter on
+                        .waitSeconds( wait )
+                        .back(40)
+                        .waitSeconds((3))
+                        //Shoot here
+                        .lineToLinearHeading(new Pose2d(-11,-25,Math.toRadians(270)))
+                        .waitSeconds(.5)
+                        //Turn intake on
+                        .forward(7)
+                        .waitSeconds(.1)
+                        .forward(7)
+                        .waitSeconds(.1)
+                        .forward(9)
+                        .waitSeconds(1)
+                        //Intake off
+                        .lineToLinearHeading(new Pose2d(-33,-14,Math.toRadians(230)))
+                        .waitSeconds(3)
+                        //Shoot again
+                        .lineToLinearHeading(new Pose2d(12,-25,Math.toRadians(270)))
+                        //Intake on
+                        .forward(7)
+                        .waitSeconds(.1)
+                        .forward(7)
+                        .waitSeconds(.1)
+                        .forward(9)
+                        .waitSeconds(1)
+                        .lineToLinearHeading(new Pose2d(-33,-14,Math.toRadians(230)))
+                        .waitSeconds(3)
+                        //Shoot
+                        .strafeLeft(25)
+                        .waitSeconds(2)
+                        .build());
+
+        return path;
+    }
+    //==============================================
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+        meepMeep.setMouseCoordinateDisplayPosition( 20, 200 );
+        meepMeep.setShowFPS( false );
+
+        RoadRunnerBotEntity path = examplePath( meepMeep );
+
+        File file = new File("Simulator/decodeField.png");
+        try
+        {
+            BufferedImage image = ImageIO.read( file );
+            meepMeep.setBackground( image )
+                    .setDarkMode( true )
+                    .setBackgroundAlpha( 0.7f )
+                    .addEntity( path )
+                    .start();
+        }
+        catch(Exception e)
+        {
+            System.out.println( e.getMessage() );
+        }
+    }
+}
